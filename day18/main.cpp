bd0be949d33ff2030e5be76450079f8536087a0e
Petr Faltýnek <pfaltynek@gmail.com>
2020-11-11 17:35:35 +0100

    day18 - part2 finished

--- day18/main.cpp
+++ day18/main.cpp
@@ -13,12 +13,6 @@ struct edge {
 	uint32_t keys_mask;
 	uint32_t keys_meet;
 };
-struct location_info {
-	coord_str pos;
-	uint64_t steps;
-	uint32_t keys_mask;
-	uint32_t keys_meet;
-};
 
 struct maze_state {
 	uint64_t steps;
@@ -50,13 +44,16 @@ class AoC2019_day18 : public AoC {
 	void analyze_maze();
 	uint32_t get_key(const char key);
 	void add_key(const char key, uint32_t& keys);
+	uint32_t pack_chars(const char c1, const char c2, const char c3, const char c4);
+	void unpack_chars(const uint32_t packed, char& c1, char& c2, char& c3, char& c4);
+	void modify_input_for_part2();
 
 	std::map<coord_str, char> map_;
 	std::map<char, coord_str> keys_map_, doors_map_;
-	coord_str entrance_;
+	std::vector<coord_str> entrances_;
 	uint32_t keys_mask_;
 	std::string keys_;
-	std::map<char, std::map<char, edge>> edges_;
+	std::vector<std::map<char, std::map<char, edge>>> edges_;
 };
 
 bool AoC2019_day18::init(const std::vector<std::string> lines) {
@@ -68,6 +65,7 @@ bool AoC2019_day18::init(const std::vector<std::string> lines) {
 	doors_map_.clear();
 	keys_.clear();
 	keys_mask_ = 0;
+	entrances_.clear();
 
 	for (uint32_t i = 0; i < lines.size(); i++) {
 		if (!i) {
@@ -85,7 +83,7 @@ bool AoC2019_day18::init(const std::vector<std::string> lines) {
 				map_[{static_cast<int32_t>(j), static_cast<int32_t>(i)}] = c;
 			} else if (c == '@') {
 				map_[{static_cast<int32_t>(j), static_cast<int32_t>(i)}] = c;
-				entrance_ = {static_cast<int32_t>(j), static_cast<int32_t>(i)};
+				entrances_.push_back({static_cast<int32_t>(j), static_cast<int32_t>(i)});
 				entrance_found = true;
 			} else if (islower(c)) {
 				map_[{static_cast<int32_t>(j), static_cast<int32_t>(i)}] = c;
@@ -117,6 +115,27 @@ uint32_t AoC2019_day18::get_key(const char key) {
 	int shift = tolower(key) - 'a';
 
 	return (0x00000001 << shift);
+}
+
+uint32_t AoC2019_day18::pack_chars(const char c1, const char c2, const char c3, const char c4) {
+	uint32_t result = 0;
+
+	result = c4;
+	result = result << 8;
+	result |= c3;
+	result = result << 8;
+	result |= c2;
+	result = result << 8;
+	result |= c1;
+
+	return result;
+}
+
+void AoC2019_day18::unpack_chars(const uint32_t packed, char& c1, char& c2, char& c3, char& c4) {
+	c1 = packed & 0x000000FF;
+	c2 = (packed >> 8) & 0x000000FF;
+	c3 = (packed >> 16) & 0x000000FF;
+	c4 = (packed >> 24) & 0x000000FF;
 }
 
 int32_t AoC2019_day18::get_aoc_day() {
@@ -162,31 +181,54 @@ void AoC2019_day18::tests() {
 			// 81 steps; one is: a, c, f, i, d, g, b, e, h
 		}
 	}
+
+	if (init({"#######", "#a.#Cd#", "##...##", "##.@.##", "##...##", "#cB#Ab#", "#######"})) {
+		modify_input_for_part2();
+		if (collect_all_keys_shortest_way(path, steps)) {
+			// 8 steps; one is: a, b, c, d
+		}
+	}
+
+	if (init({"#############", "#DcBa.#.GhKl#", "#.###@#@#I###", "#e#d#####j#k#", "###C#@#@###J#", "#fEbA.#.FgHi#", "#############"})) {
+		// modify_input_for_part2();
+		if (collect_all_keys_shortest_way(path, steps)) {
+			// 32 steps; one is: a, b, c, d, e, f, g, h, i, j, k, l
+		}
+	}
+
+	if (init({"#############", "#g#f.D#..h#l#", "#F###e#E###.#", "#dCba@#@BcIJ#", "#############", "#nK.L@#@G...#", "#M###N#H###.#", "#o#m..#i#jk.#",
+			  "#############"})) {
+		// modify_input_for_part2();
+		if (collect_all_keys_shortest_way(path, steps)) {
+			// 72 steps; one is: e, h, i, a, b, c, d, f, g, k, j, l, n, m, o
+		}
+	}
 }
 
 void AoC2019_day18::analyze_maze() {
 	edge e;
 	edges_.clear();
 
-	e.from = '@';
-	for (uint32_t i = 0; i < keys_.size(); i++) {
-		e.to = keys_[i];
-		if (find_shortest_path_to_key(entrance_, e)) {
-			edges_[e.from][e.to] = e;
-		} else {
-			int x = 13;
+	for (uint32_t en = 0; en < entrances_.size(); ++en) {
+		edges_.push_back({});
+		e.from = '@';
+		for (uint32_t i = 0; i < keys_.size(); i++) {
+			e.to = keys_[i];
+			if (find_shortest_path_to_key(entrances_[en], e)) {
+				edges_[en][e.from][e.to] = e;
+			}
 		}
 	}
 
-	for (uint32_t i = 0; i < keys_.size(); i++) {
-		e.from = keys_[i];
-		for (uint32_t j = i + 1; j < keys_.size(); j++) {
-			e.to = keys_[j];
-			if (find_shortest_path_to_key(keys_map_[e.from], e)) {
-				edges_[e.from][e.to] = e;
-				edges_[e.to][e.from] = e;
-			} else {
-				int x = 13;
+	for (uint32_t en = 0; en < entrances_.size(); ++en) {
+		for (uint32_t i = 0; i < keys_.size(); i++) {
+			e.from = keys_[i];
+			for (uint32_t j = i + 1; j < keys_.size(); j++) {
+				e.to = keys_[j];
+				if (find_shortest_path_to_key(keys_map_[e.from], e)) {
+					edges_[en][e.from][e.to] = e;
+					edges_[en][e.to][e.from] = e;
+				}
 			}
 		}
 	}
@@ -195,21 +237,20 @@ void AoC2019_day18::analyze_maze() {
 bool AoC2019_day18::collect_all_keys_shortest_way(std::string& path, uint64_t& steps) {
 	bool result_found = false;
 	maze_state state, next;
-	std::map<char, std::map<uint32_t, maze_state>> q1 = {}, q2 = {};
+	std::map<uint32_t, std::map<uint32_t, maze_state>> q1 = {}, q2 = {};
 	std::map<uint32_t, maze_state> m;
-	char from, to;
-	uint32_t key_bit, keys_having, keys_having_new, keys_required, keys_on_the_way;
+	char to;
+	char c[4];
+	uint32_t key_bit, keys_having, keys_having_new, keys_required, keys_on_the_way, chars_packed;
+	edge e;
 
-	q1['@'][0] = {0, ""};
+	q1[pack_chars('@', '@', '@', '@')][0] = {0, ""};
 
 	do {
 		for (auto it1 = q1.begin(); it1 != q1.end(); it1++) {
-			from = it1->first;
-			if (!edges_.count(from)) {
-				continue;
-			}
-
+			unpack_chars(it1->first, c[0], c[1], c[2], c[3]);
 			m = it1->second;
+
 			for (auto it2 = m.begin(); it2 != m.end(); it2++) {
 				keys_having = it2->first;
 				state = it2->second;
@@ -226,40 +267,46 @@ bool AoC2019_day18::collect_all_keys_shortest_way(std::string& path, uint64_t& s
 					continue;
 				}
 
-				for (uint32_t i = 0; i < keys_.size(); i++) {
-					to = keys_[i];
-					if (!edges_[from].count(to)) {
-						continue;
-					}
+				for (uint8_t i = 0; i < entrances_.size(); i++) {
+					if (edges_[i].count(c[i])) {
+						for (auto it2 = edges_[i][c[i]].begin(); it2 != edges_[i][c[i]].end(); it2++) {
+							to = it2->first;
+							e = it2->second;
 
-					key_bit = get_key(to);
-					if (key_bit & keys_having) {
-						continue;
-					}
-
-					keys_required = edges_[from][to].keys_mask;
-					if ((keys_required & keys_having) != keys_required) {
-						continue;
-					}
-					keys_on_the_way = edges_[from][to].keys_meet;
-					if ((keys_having & keys_on_the_way) != keys_on_the_way) {
-						continue;
-					}
-					next = state;
-					keys_having_new = keys_having | key_bit;
-					next.path += to;
-					next.steps += edges_[from][to].steps;
-
-					if (q2.count(to)) {
-						if (q2[to].count(keys_having_new)) {
-							if (q2[to][keys_having_new].steps > next.steps) {
-								q2[to][keys_having_new] = next;
+							key_bit = get_key(to);
+							if (key_bit & keys_having) {
+								continue;
 							}
-						} else {
-							q2[to][keys_having_new] = next;
+
+							keys_required = e.keys_mask;
+							if ((keys_required & keys_having) != keys_required) {
+								continue;
+							}
+							keys_on_the_way = e.keys_meet;
+							if ((keys_having & keys_on_the_way) != keys_on_the_way) {
+								continue;
+							}
+							next = state;
+							keys_having_new = keys_having | key_bit;
+							next.path += to;
+							next.steps += e.steps;
+
+							std::swap(c[i], to);
+							chars_packed = pack_chars(c[0], c[1], c[2], c[3]);
+							std::swap(c[i], to);
+
+							if (q2.count(chars_packed)) {
+								if (q2[chars_packed].count(keys_having_new)) {
+									if (q2[chars_packed][keys_having_new].steps > next.steps) {
+										q2[chars_packed][keys_having_new] = next;
+									}
+								} else {
+									q2[chars_packed][keys_having_new] = next;
+								}
+							} else {
+								q2[chars_packed][keys_having_new] = next;
+							}
 						}
-					} else {
-						q2[to][keys_having_new] = next;
 					}
 				}
 			}
@@ -272,6 +319,13 @@ bool AoC2019_day18::collect_all_keys_shortest_way(std::string& path, uint64_t& s
 }
 
 bool AoC2019_day18::find_shortest_path_to_key(const coord_str start_pos, edge& e) {
+	struct location_info {
+		coord_str pos;
+		uint64_t steps;
+		uint32_t keys_mask;
+		uint32_t keys_meet;
+	};
+
 	std::queue<location_info> q;
 	std::set<coord_str> history = {};
 	uint32_t keys, meet;
@@ -321,6 +375,29 @@ bool AoC2019_day18::find_shortest_path_to_key(const coord_str start_pos, edge& e
 	return false;
 }
 
+void AoC2019_day18::modify_input_for_part2() {
+	coord_str entrance = entrances_[0];
+	map_.erase(entrance);
+	map_.erase(entrance + coord_step_north);
+	map_.erase(entrance + coord_step_south);
+	map_.erase(entrance + coord_step_east);
+	map_.erase(entrance + coord_step_west);
+	map_[entrance + coord_step_north + coord_step_west] = '@';
+	map_[entrance + coord_step_north + coord_step_east] = '@';
+	map_[entrance + coord_step_south + coord_step_west] = '@';
+	map_[entrance + coord_step_south + coord_step_east] = '@';
+
+	entrances_.clear();
+	entrances_ = {entrance + coord_step_north + coord_step_west, entrance + coord_step_north + coord_step_east, entrance + coord_step_south + coord_step_west,
+				  entrance + coord_step_south + coord_step_east};
+
+	for (uint8_t i = 0; i < entrances_.size(); i++) {
+		map_[entrances_[i]] = '@';
+	}
+
+	analyze_maze();
+}
+
 bool AoC2019_day18::part1() {
 	std::string path;
 	uint64_t result = 0;
@@ -334,8 +411,12 @@ bool AoC2019_day18::part1() {
 }
 
 bool AoC2019_day18::part2() {
-	int64_t result = 0;
-	if (1) {
+	uint64_t result = 0;
+	std::string path;
+
+	modify_input_for_part2();
+
+	if (collect_all_keys_shortest_way(path, result)) {
 		result2_ = std::to_string(result);
 		return true;
 	}
