b7c51dc33e09966f277c1ab5970d37ef0743246b
Petr Faltýnek <pfaltynek@gmail.com>
2021-07-02 11:58:56 +0200

    day22 finished, big redesign, see hints.md

---
+++ day22/hints.md
@@ -0,0 +1,110 @@
+Great explanation by sasa1977 from https://github.com/sasa1977/aoc/blob/master/lib/2019/201922.ex
+
+# Part 1
+
+## Cyclical space
+
+The key insight is that a deck of card can be repeated cyclically. So for example, if we have cards 0, 1, and 2,
+we could represent the deck as 0, 1, 2, 0, 1, 2, 0, 1, 2, ...
+
+In such representation, the card at position 0 is the same as the card at position 3 is the same as the card at
+position 6, and so on. Likewise, the indices can stretch to negatives, so for example the card at position 2
+is the same as the card at position -1, is the same as the card at position -4, and so on.
+
+More generally, we can say that pos == pos + n * deck_size for any arbitrary integer n.
+
+## Linear transformations
+
+Given such representation, we can express each transformation as a linear function that takes the initial position
+as its argument and returns the new position:
+
+- deal into new stack: new_pos = -current_pos + deck_size - 1
+- cut: new_pos = current_pos - cut_pos
+- deal with increment: new_pos = increment * current_pos
+
+Note that these functions can return positions which are not in the range 0..(deck_size - 1), but we can easily
+normalize the resulting positions with `rem(pos, deck_size)` for zero and positive positions, or
+`deck_size - rem(-pos, deck_size)` for negative positions.
+
+Notice that all of the listed functions are linear, i.e. they can be represented as a*x + b.
+This means that the entire shuffle sequence can be expressed as a single function which is a composition of
+all the steps.
+
+For example, suppose that we have only three steps in the sequence, called f, g, h. Then the entire transformation
+can be represented as f(g(h(current_pos))). We can build this composition iteratively by composing g and h, and then
+composing f with the result. Given two functions f = a*x+b and g = c*x+d, composition g(f(x)) is c*a*x + c*b + d.
+
+This is all we need to solve part 1. We read the input, and build a collection of `{a, b}` pairs which represent
+each step as a function. Then we build a composition of these steps to produce the function which represents the
+entire transformation. Finally, we apply the function by simply calculating a*2019+b to get the new
+position of 2019 after the entire shuffle has been performed. Remember that we need to normalize the result
+to fall in the range of 0..(deck_size - 1).
+
+# Part 2
+
+## Normalizing functions
+
+One challenge in part 2 is that we have to iteratively apply the function many times. The problem we'll encounter
+here is that a and b values in the function are going to be very large. Applying the function iteratively will
+generate even larger numbers. Although in Elixir numbers are bound only by memory, this will still not work, because
+calculations will become too slow.
+
+Therefore, we need to normalize a and b. As it turns out, this can be done quite easily. In our cyclical space,
+a function a*pos+b will generate the same position (after normalization) as the function
+(a + n*deck_size)*x + b + m*deck_size for any arbitrary pair of integers n and m. In other words, we can
+arbitrarily (and independently!) increment or decrement a and b by some multiple of deck_size.
+
+I have no clue which theorem defines this, but it can be informally proven. Consider again the expanded version
+(a + n*deck_size)*x + b + m*deck_size. This can be rewritten as a*x + b + deck_size * (n*x + b + m).
+We know that (n*x + b + m) is an integer (since all values are integers), and we know from before that
+pos == pos + n * deck_size for any integer n, which means that these two functions will always produce the same
+position (again, after the output positions are normalized).
+
+Armed with that knowledge, we can normalize each function `{a, b}` into
+`{normalize(a, deck_size), normalize(b, deck_size)}`, where `normalize` is basically `rem` with a special handling
+of negative values, as explained earlier.
+
+To summarize, when we compose functions, we'll normalize the vectors. Likewise, after computing new positions,
+we'll normalize their values. Thus, all the values we work with will always be in the range of 0..(deck_size - 1).
+
+## Inverting the direction
+
+Another challenge of part 2 is that we have to find the value that ends up in position 2020. However, our function
+works in the opposite direction - it computes the new position from the previous one. To solve this, we need
+to inverse the shuffle function.
+
+An inverse of a linear function a*x + b is 1/a - b/a (obtained by swapping x and y in the original function
+definition, and transforming to standard representation). To make sure we don't end up with floats,
+we need to increase 1 and b (by repeatedly adding deck_size) to make sure the inverse function
+coefficients are still integers.
+
+With these ideas in place, the inverse of the shuffle is computed as follows:
+
+1. Read the input
+2. Produce functions as in part 1
+3. Reverse the list of functions
+4. Invert each function
+5. Calculate the composition as in part 1
+
+The output is again a function which works in the opposite direction. It takes `next_pos` as its argument and returns
+`previous_pos`.
+
+## Applying the function many times
+
+To apply the function n times, we can simply compose it with it self. A shuffle applied 4 times is f(f(f(f(x)))).
+Unfortunately, the number of steps is quite large (101_741_582_076_661), so this won't finish in a reasonable
+amount of time. To speed things up, we can use the technique called exponentiation by squaring
+(https://en.wikipedia.org/wiki/Exponentiation_by_squaring).
+
+For example to produce the function which performs the shuffle sequence 100 times, we can do the following:
+
+1. f2(x) = f(f(x))
+2. f4(x) = f2(f2(x))
+3. f8(x) = f4(f4(x))
+4. f16(x) = f8(f8(x))
+5. f32(x) = f16(f16(x))
+6. f64(x) = f32(f32(x))
+7. f100(x) = f64(f32(f4(x)))
+
+So instead of performing 100 compositions, we only did 7. For 101_741_582_076_661 steps we'll only need about
+100 compositions, which can be done quickly.
--- day22/main.cpp
+++ day22/main.cpp
@@ -4,13 +4,18 @@
 #include <array>
 #include <regex>
 
+typedef __uint128_t uint128_t;
+typedef __int128_t int128_t;
+
 const std::regex C_DEAL_NEW_STACK("^deal into new stack$");
 const std::regex C_CUT_CARDS("^cut (-?\\d+)$");
 const std::regex C_DEAL_INCREMENT("deal with increment (\\d+)$");
-const uint64_t C_MAX_CARDS_PART1 = 10007;
+const uint128_t C_MAX_CARDS_PART1 = 10007;
+const uint128_t C_MAX_CARDS_PART2 = 119315717514047;
+const uint128_t C_REPETITIONS_PART2 = 101741582076661;
 
 typedef enum DEAL_TYPE { DEAL_NEW, DEAL_INCREMENT, DEAL_CUT } deal_type_t;
-
+typedef std::pair<int128_t, int128_t> koef_pair_t;
 class AoC2019_day22 : public AoC {
   protected:
 	bool init(const std::vector<std::string> lines);
@@ -21,13 +26,25 @@ class AoC2019_day22 : public AoC {
 	int32_t get_aoc_year();
 
   private:
-	std::vector<std::pair<deal_type_t, int64_t>> deals_;
-	std::array<uint64_t, C_MAX_CARDS_PART1> cards_, tmp_cards_;
-	void shuffle_cards(const uint64_t cards_cnt);
+	std::vector<std::pair<deal_type_t, int128_t>> deals_;
+	std::vector<koef_pair_t> koefs_;
+	int128_t a_, b_;
+
+	void find_linear_transformation_koefs(const int128_t deck_size);
+	int128_t get_new_card_position(const int128_t card, const int128_t deck_size);
+	int128_t calculate_card_pos(const int128_t card, const int128_t deck_size);
+	int128_t get_card_number_of_position(const int128_t position, const int128_t repetitions, const int128_t deck_size);
+	int128_t normalize(const int128_t result, const int128_t deck_size);
+	int128_t normalize_fragment(const int128_t numerator, const int128_t denominator, const int128_t deck_size);
+	void find_composite_function_koefs(const int128_t deck_size);
+	void invert_function_koefs(const int128_t deck_size);
+	void reduce_repetitions(const int128_t repeat_cnt, const int128_t deck_size);
+	koef_pair_t compose(const int128_t a1, const int128_t b1, const int128_t a2, const int128_t b2, const int128_t deck_size);
+	void prepare_koefs_part2(const int128_t repeat_cnt, const int128_t deck_size);
 };
 
 bool AoC2019_day22::init(const std::vector<std::string> lines) {
-	int64_t value;
+	int128_t value;
 	std::smatch sm;
 
 	deals_.clear();
@@ -50,114 +67,285 @@ bool AoC2019_day22::init(const std::vector<std::string> lines) {
 	return true;
 }
 
-void AoC2019_day22::shuffle_cards(const uint64_t cards_count) {
-	int64_t value, count, rest_count;
-	uint64_t next_position = 0;
-	uint64_t *from1, *from2, *to1, *to2;
+int128_t AoC2019_day22::get_new_card_position(const int128_t card, const int128_t deck_size) {
+	find_linear_transformation_koefs(deck_size);
+	find_composite_function_koefs(deck_size);
+	return calculate_card_pos(card, deck_size);
+}
 
-	for (size_t i = 0; i < cards_count; i++) {
-		cards_[i] = i;
-	}
+void AoC2019_day22::find_linear_transformation_koefs(const int128_t deck_size) {
+	int128_t value, a, b;
+
+	koefs_.clear();
 
 	for (size_t i = 0; i < deals_.size(); i++) {
 		value = deals_[i].second;
 
+		a = b = 0;
+
 		switch (deals_[i].first) {
 			case DEAL_NEW:
-				std::reverse(cards_.begin(), cards_.begin() + cards_count);
+				a = -1;
+				b = deck_size - 1;
 				break;
 			case DEAL_INCREMENT:
-				next_position = 0;
-
-				for (size_t j = 0; j < cards_count; j++) {
-					tmp_cards_[next_position] = cards_[j];
-					next_position = (next_position + value) % cards_count;
-				}
-
-				cards_.swap(tmp_cards_);
+				a = value;
+				b = 0;
 				break;
 			case DEAL_CUT:
-				to1 = to2 = tmp_cards_.data();
-				from1 = from2 = cards_.data();
-
-				count = abs(value);
-				rest_count = cards_count - count;
-
-				if (value > 0) {
-					to1 = to1 + rest_count;
-					from2 = from2 + count;
-				} else if (value < 0) {
-					from1 = from1 + rest_count;
-					to2 = to2 + count;
-				}
-
-				memcpy(to1, from1, count * sizeof(uint64_t));
-				memcpy(to2, from2, rest_count * sizeof(uint64_t));
-
-				cards_.swap(tmp_cards_);
+				a = 1;
+				b = -value;
 				break;
 			default:
 				int z = 13;
 				break;
 		}
+		koefs_.push_back({a, b});
 	}
 }
 
+void AoC2019_day22::find_composite_function_koefs(const int128_t deck_size) {
+	int128_t a, b, c, d;
+
+	if (koefs_.size()) {
+		a = koefs_[0].first;
+		b = koefs_[0].second;
+
+		for (size_t i = 1; i < koefs_.size(); i++) {
+			c = koefs_[i].first;
+			d = koefs_[i].second;
+			auto p = compose(a, b, c, d, deck_size);
+			a = p.first;
+			b = p.second;
+		}
+		a_ = a;
+		b_ = b;
+	}
+}
+
+int128_t AoC2019_day22::calculate_card_pos(const int128_t card, const int128_t deck_size) {
+	return normalize(a_ * card + b_, deck_size);
+}
+
+int128_t AoC2019_day22::normalize(const int128_t result, const int128_t deck_size) {
+	if (result >= 0) {
+		return result % deck_size;
+	} else {
+		return deck_size - (-result % deck_size);
+	}
+}
+
+int128_t AoC2019_day22::normalize_fragment(const int128_t numerator, const int128_t denominator, const int128_t deck_size) {
+	int128_t value = numerator;
+
+	while (value % denominator) {
+		value += deck_size;
+	}
+
+	return value / denominator;
+}
+
+koef_pair_t AoC2019_day22::compose(const int128_t a1, const int128_t b1, const int128_t a2, const int128_t b2, const int128_t deck_size) {
+	return {normalize(a1 * a2, deck_size), normalize(a2 * b1 + b2, deck_size)};
+}
+
+void AoC2019_day22::invert_function_koefs(const int128_t deck_size) {
+	std::vector<koef_pair_t> inv = {};
+	int128_t a, b, ai, bi;
+
+	for (size_t i = 0; i < koefs_.size(); i++) {
+		a = koefs_[i].first;
+		b = koefs_[i].second;
+		ai = normalize_fragment(1, a, deck_size);
+		bi = normalize_fragment(-b, a, deck_size);
+		inv.push_back({ai, bi});
+	}
+
+	koefs_.swap(inv);
+}
+
+void AoC2019_day22::reduce_repetitions(const int128_t repeat_cnt, const int128_t deck_size) {
+	int128_t a, b, aa, bb, pos = 1;
+	bool found = (repeat_cnt % 2) != 0;
+
+	a = aa = a_;
+	b = bb = b_;
+
+	while (pos <= repeat_cnt) {
+		auto p = compose(a, b, a, b, deck_size);
+		a = p.first;
+		b = p.second;
+		pos = pos << 1;
+
+		if ((pos & repeat_cnt) != 0) {
+			if (!found) {
+				aa = a;
+				bb = b;
+				found = true;
+			} else {
+				auto p = compose(a, b, aa, bb, deck_size);
+				aa = p.first;
+				bb = p.second;
+			}
+		}
+	}
+
+	a_ = aa;
+	b_ = bb;
+}
+
+void AoC2019_day22::prepare_koefs_part2(const int128_t repeat_cnt, const int128_t deck_size) {
+
+	find_linear_transformation_koefs(deck_size);
+	std::reverse(koefs_.begin(), koefs_.end());
+	invert_function_koefs(deck_size);
+	find_composite_function_koefs(deck_size);
+
+	if (repeat_cnt > 1) {
+		reduce_repetitions(repeat_cnt, deck_size);
+	}
+}
+
+int128_t AoC2019_day22::get_card_number_of_position(const int128_t position, const int128_t repetitions, const int128_t deck_size) {
+	prepare_koefs_part2(repetitions, deck_size);
+	return calculate_card_pos(position, deck_size);
+}
+
 void AoC2019_day22::tests() {
+	std::vector<int128_t> result1(10), result2(10);
 
 	if (init({"deal into new stack"})) {
-		shuffle_cards(10); // 9 8 7 6 5 4 3 2 1 0
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"cut 3"})) {
-		shuffle_cards(10); // 3 4 5 6 7 8 9 0 1 2
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"cut -4"})) {
-		shuffle_cards(10); // 6 7 8 9 0 1 2 3 4 5
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"deal with increment 3"})) {
-		shuffle_cards(10); // 0 7 4 1 8 5 2 9 6 3
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"deal with increment 7", "deal into new stack", "deal into new stack"})) {
-		shuffle_cards(10); // 0 3 6 9 2 5 8 1 4 7
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"cut 6", "deal with increment 7", "deal into new stack"})) {
-		shuffle_cards(10); // 3 0 7 4 1 8 5 2 9 6
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"deal with increment 7", "deal with increment 9", "cut -2"})) {
-		shuffle_cards(10); // 6 3 0 7 4 1 8 5 2 9
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 
 	if (init({"deal into new stack", "cut -2", "deal with increment 7", "cut 8", "cut -4", "deal with increment 7", "cut 3", "deal with increment 9",
 			  "deal with increment 3", "cut -1"})) {
-		shuffle_cards(10); // 9 2 5 8 1 4 7 0 3 6
+		find_linear_transformation_koefs(10);
+		find_composite_function_koefs(10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result1[calculate_card_pos(i, 10)] = i;
+		}
+
+		prepare_koefs_part2(1, 10);
+
+		for (size_t i = 0; i < 10; i++) {
+			result2[result1[i]] = calculate_card_pos(i, 10);
+		}
 	}
 }
 
 bool AoC2019_day22::part1() {
-	std::array<uint64_t, C_MAX_CARDS_PART1>::iterator pos;
 
-	shuffle_cards(C_MAX_CARDS_PART1);
-
-	pos = std::find(cards_.begin(), cards_.end(), 2019);
-
-	if (pos != cards_.end()) {
-		result1_ = std::to_string(pos - cards_.begin());
-	} else {
-		return false;
-	}
+	result1_ = std::to_string((uint64_t)get_new_card_position(2019, C_MAX_CARDS_PART1));
 
 	return true;
 }
 
 bool AoC2019_day22::part2() {
 
-	result2_ = std::to_string(0);
+	result2_ = std::to_string((uint64_t)get_card_number_of_position(2020, C_REPETITIONS_PART2, C_MAX_CARDS_PART2));
 
 	return true;
 }
